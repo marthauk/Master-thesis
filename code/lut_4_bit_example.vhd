2f769f3dcd4cc52639db0bae8987b68a91ce8116
marthauk <martinhaukali@gmail.com>
2018-05-30 08:54:54 +0200

    Updates from ShareLaTeX

--- code/lut_4_bit_example.vhd
+++ code/lut_4_bit_example.vhd
@@ -9,7 +9,8 @@ end division_lut;
 architecture rtl of division_lut is
 constant C_NY       : integer:= 4;
 constant C_NDY      : integer:= 4;
-type t_divition_lut is array (0 to 2**C_NY-1) of integer range 0 to 2**C_NDY-1;
+type t_divition_lut is array (0 to 2**C_NY-1) of 
+integer range 0 to 2**C_NDY-1;
 constant C_DIV_LUT  : t_divition_lut := (
   16,
   8,
@@ -29,6 +30,7 @@ constant C_DIV_LUT  : t_divition_lut := (
   1
    );
 begin
-  y_inv <= std_logic_vector(to_unsigned(C_DIV_LUT(to_integer(unsigned(y))),C_NDY));
+  y_inv <= std_logic_vector(to_unsigned(
+    C_DIV_LUT(to_integer(unsigned(y))),C_NDY));
     
 end rtl;
